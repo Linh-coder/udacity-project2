8a3fe700d67e420aea5d31bb41070bd4bd48ae9f
linhtran <linhtran2391999@gmail.com>
2023-11-04 15:57:36 +0700

    init project

---
+++ udacity-neighborly-app-master/NeighborlyAPI/getAdvertisements/__init__.py
@@ -0,0 +1,25 @@
+import logging
+import os
+import azure.functions as func
+import pymongo
+import json
+from bson.json_util import dumps
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    try:
+        url = os.environ["MyConnectionString"]  # TODO: Update with appropriate MongoDB connection information
+        client = pymongo.MongoClient(url)
+        database = client['neighbordb']
+        collection = database['advertisements']
+
+
+        result = collection.find({})
+        result = dumps(result)
+
+        return func.HttpResponse(result, mimetype="application/json", charset='utf-8')
+    except:
+        print("could not connect to mongodb")
+        return func.HttpResponse("could not connect to mongodb",
+                                 status_code=400)
+
